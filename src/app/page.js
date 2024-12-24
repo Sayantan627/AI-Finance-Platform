dd26d8005fb77d1ea011943b43c9abae1a962270
Sayantan_627 <27sayantan.pal@gmail.com>
2024-12-24 19:11:41 +0530

    initial setup

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl text-blue-300">Welth</h1>
+      <Button variant="secondary" className="m-4">
+        Click Me
+      </Button>
+    </>
+  );
+}
