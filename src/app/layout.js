0e3beda1ae2f1ea73e41db400fe7fa904c66f004
Sayantan_627 <27sayantan.pal@gmail.com>
2024-12-25 20:51:06 +0530

    Add authentication layout and components with Clerk integration

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,18 +12,20 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-        {/* header */}
-        <Header />
-        <main className="min-h-screen">{children}</main>
-        {/* footer */}
-        <footer className="bg-blue-50 py-12">
-          <div className="container mx-auto text-center text-gray-600 px-4">
-            <p>Made with 💗 by Sayantan</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} antialiased`}>
+          {/* header */}
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          {/* footer */}
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto text-center text-gray-600 px-4">
+              <p>Made with 💗 by Sayantan</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,16 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import { SignedOut, SignInButton } from "@clerk/nextjs";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="top-0 fixed">
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
 };
 export default Header;
