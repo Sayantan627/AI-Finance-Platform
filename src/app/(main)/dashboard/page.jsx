78f0476d769cc3f6e0208060c6f89b24c73bebbe
Sayantan_627 <27sayantan.pal@gmail.com>
2025-01-07 20:40:34 +0530

    Add account schema, custom fetch hook, and Toaster component

--- src/app/(main)/dashboard/page.jsx
+++ src/app/(main)/dashboard/page.jsx
@@ -1,3 +1,7 @@
+import CreateAccountDrawer from "@/components/create-account-drawer";
+import { Card, CardContent } from "@/components/ui/card";
+import { Plus } from "lucide-react";
+
 const DashboardPage = () => {
   return (
     <div className="px-5">
@@ -6,6 +10,16 @@ const DashboardPage = () => {
       {/* Overview */}
 
       {/* Accounts Grid */}
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <CreateAccountDrawer>
+          <Card className="border-dashed hover:shadow-md cursor-pointer transition-shadow">
+            <CardContent className="flex flex-col items-center justify-center text-muted-foreground pt-5 h-full">
+              <Plus className="h-10 w-10 mb-2" />
+              <p className="font-medium text-sm capitalize">add new account</p>
+            </CardContent>
+          </Card>
+        </CreateAccountDrawer>
+      </div>
     </div>
   );
 };
