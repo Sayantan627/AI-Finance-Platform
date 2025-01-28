f1ad9e30b7d2a7a719586d52c919acddc3fb59d4
Sayantan_627 <27sayantan.pal@gmail.com>
2025-01-28 16:18:19 +0530

    Enable dynamic rendering in ClerkProvider within RootLayout component

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider dynamic>
       <html lang="en">
         <body className={`${inter.className} antialiased`}>
           {/* header */}
